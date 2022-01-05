50c0deb0ae7bc5e875439f72c07e39c8c60d3e63
amyjprogrammer <amyjprogrammer@gmail.com>
2022-01-04 23:01:07 -0500

    Add project files.

---
+++ MvxStarter.Core/Models/PersonModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MvxStarter.Core.Models
+{
+    public class PersonModel
+    {
+        public int FirstName { get; set; }
+        public int LastName { get; set; }
+    }
+}
